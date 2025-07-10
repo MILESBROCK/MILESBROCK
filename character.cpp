3d06eb12f8ee4fe60515e4e2a6d7c6ae3a1e2284
Jayanthan.M <91411407+MILESBROCK@users.noreply.github.com>
2025-07-10 14:47:03 +0530

    Create character.cpp

---
+++ character.cpp
@@ -0,0 +1,45 @@
+// MyCharacter.cpp
+#include "MyCharacter.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+AMyCharacter::AMyCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMyCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMyCharacter::StartJump);
+    PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMyCharacter::StopJump);
+}
+
+void AMyCharacter::MoveForward(float Value)
+{
+    AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void AMyCharacter::MoveRight(float Value)
+{
+    AddMovementInput(GetActorRightVector() * Value);
+}
+
+void AMyCharacter::StartJump()
+{
+    bPressedJump = true;
+}
+
+void AMyCharacter::StopJump()
+{
+    bPressedJump = false;
+}
